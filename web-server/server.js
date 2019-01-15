7840ca2f679485659a2152fd39af301e907a22bb
ORTIJ262 <Juan.Pablo.X.Ortiz.Palacio.-ND@disney.com>
2019-01-14 21:56:18 -0500

    Add refactor to middleware component

--- web-server/server.js
+++ web-server/server.js
@@ -1,15 +1,7 @@
 var express = require('express');
+var middleware = require('./middleware.js');
 var app = express();
-var middleware = {
-    requireAuthentication: function (req, res, next) {
-        console.log('Private route hit');
-        next();
-    },
-    logger: function (req, res, next) {
-        console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
-        next();
-    }
-};
+
 
 const PORT = 3000;
 
